17941c4ff2c27bff0717436d0d30896f82d9ba29
bfrasholb <13065@holbertonstudents.com>
2026-04-07 15:04:02 +1000

    Betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,12 +8,6 @@
 
 void selection_sort(int *array, size_t size)
 {
-/**
- * 1. initalise selector
- * 2. set selector = a[0]
- * 3. while (array){ if < selector then swap selector and array[current],}
- * 4. at the end of the array, append the selector to the sorted string
- */
 	int tmp;
 	size_t i = 0, j, min;
 
